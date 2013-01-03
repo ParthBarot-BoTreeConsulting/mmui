c2016588b318ccf93093409571a378481e57e6b4
Parth Barot <parth.barot@botreeconsulting.com>
2013-01-04 00:31:08 +0530

    adding code

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,15 @@
+class HomeController < ApplicationController
+
+  def home
+   render 'home'
+  end
+
+  def list_users
+
+  end
+
+  def send_message_to_user
+
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mmui::Application.config.secret_token = 'aec05ea260a71762b74936ad8e19383ebbef45fd1139484d3f233a22b5b58237257b8b497010fd195f72534a4bdf1b34ac096006d97e7c027da209a40b8ff478'
